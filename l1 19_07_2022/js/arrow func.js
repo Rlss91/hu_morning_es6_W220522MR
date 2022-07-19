59d458852db34b833e04b69ea1ebdd288bf9762a
Rlss91 <rlss91@gmail.com>
2022-07-19 16:00:56 +0300

    arrow functions

---
+++ l1 19_07_2022/js/arrow func.js
@@ -0,0 +1,15 @@
+// function f1(){}
+const f1 = () => {};
+
+// function f2() {
+//   return 1 + 1;
+// }
+// const f2 = () => {
+//   return 1 + 1;
+// };
+//* note that if you have only one line of code
+//* and you need to return the value you can use
+//* this syntax
+const f2 = () => 1 + 1;
+
+const f3 = (item) => 1 + 1;
