e12186979374db4deb16fb20b98d4a77de647c4f
Rlss91 <rlss91@gmail.com>
2022-07-19 15:33:49 +0300

    get elements

---
+++ l1 19_07_2022/js/getElements.js
@@ -0,0 +1,9 @@
+// let divid = document.getElementById("divid");
+console.log(divid);
+divid.innerHTML = "<h1>no get elements</h1>";
+
+let p1 = document.querySelector(".p1"); //like css
+console.log("p1", p1);
+
+let pArr = document.querySelectorAll(".p1");
+console.log("all p1s", pArr);
