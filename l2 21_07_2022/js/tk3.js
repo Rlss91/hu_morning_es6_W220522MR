90f4738b35f11ae3cfa198261a08ef886c69d1ad
Rlss91 <rlss91@gmail.com>
2022-07-21 12:13:18 +0300

    tk3

---
+++ l2 21_07_2022/js/tk3.js
@@ -0,0 +1,17 @@
+// window.addEventListener("load", function(){
+//     document.getElementById
+// })
+
+window.addEventListener("load", () => {
+  document.querySelector("h1").innerText = "Hello";
+});
+
+// const changeH1 = () => {
+//   document.querySelector("h1").innerText = "Hello";
+// };
+// changeH1();
+// window.addEventListener("load", changeH1);
+
+// (() => {
+//   document.querySelector("h1").innerText = "Hello";
+// })();
