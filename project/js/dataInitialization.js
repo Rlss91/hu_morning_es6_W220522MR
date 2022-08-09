d0e78829620e2f0d108020e1158fa392a29363d6
Rlss91 <rlss91@gmail.com>
2022-08-09 12:14:51 +0300

    more RealEstate

--- project/js/dataInitialization.js
+++ project/js/dataInitialization.js
@@ -22,6 +22,30 @@ let realEstateArr = [
     "http://127.0.0.1:5500/project/assets/imgs/pexels-pixabay-280222.jpg",
     3
   ),
+  new RealEstate(
+    "adams castle",
+    300000000,
+    "http://127.0.0.1:5500/project/assets/imgs/villa-g891a73c21_1920.jpg",
+    4
+  ),
+  new RealEstate(
+    "loft",
+    1250000,
+    "http://127.0.0.1:5500/project/assets/imgs/house-gf14c5e267_1280.jpg",
+    5
+  ),
+  new RealEstate(
+    "lake house",
+    1000000,
+    "http://127.0.0.1:5500/project/assets/imgs/lake-g901a60927_1280.jpg",
+    6
+  ),
+  new RealEstate(
+    "mountain house",
+    1500000,
+    "http://127.0.0.1:5500/project/assets/imgs/mountains-g3c8089815_1920.jpg",
+    7
+  ),
 ];
 
 const donotTouchRealEstateArr = _.cloneDeep(realEstateArr);
